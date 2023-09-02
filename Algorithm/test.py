7d3deba448554c315322a5bba712403cf5592a39
YoungIl <octover1025@naver.com>
2023-09-02 23:31:17 +0900

    0902_algo

--- Algorithm/test.py
+++ Algorithm/test.py
@@ -1,26 +1,36 @@
 import sys
+from collections import deque
 input = lambda : sys.stdin.readline().rstrip()
 
-T = int(input())
-for _ in range(T):
-    N, M = map(int, input().split())
-    nums = []
-    row_sum, col_sum = [0] * N, [0] * N
-    for i in range(N):
-        row = list(map(int, input().split()))
-        row_sum[i] = sum(row)
-        for j in range(N):
-            col_sum[j] += row[j]
-    
-    # 연산 시작
-    for _ in range(M):
-        r1, c1, r2, c2, v = map(int, input().split())
-        for i in range(r1-1, r2):
-            row_sum[i] += v * (c2 - c1 + 1)
+N = int(input())
+q = deque()
 
-        for j in range(c1-1, c2):
-            col_sum[j] += v * (r2 - r1 + 1)
-
-    print(" ".join(map(str, row_sum)))
-    print(" ".join(map(str, col_sum)))
-
+for _ in range(N):
+    order = input()
+    if order == 'size':
+        print(len(q))
+    elif order == 'pop':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q.popleft())
+    elif order == 'empty':
+        if len(q) == 0:
+            print(1)
+        else:
+            print(0)
+    elif order == 'front':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q[0])
+    elif order == 'back':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q[-1])
+    else:
+        temp = list(map(str, input().split()))
+        print(temp)
+        num = temp[1]
+        q.append(int(num))
