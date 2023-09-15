071e22e54db10e1d39fd38aa338e2197095c0a7c
YoungIl <octover1025@naver.com>
2023-09-15 20:03:42 +0900

    0915_algo

--- Algorithm/test.py
+++ Algorithm/test.py
@@ -1,36 +1,6 @@
-import sys
+import sys, re
 input = lambda : sys.stdin.readline().rstrip()
 
-L = int(input())
-S = sorted(list(map(int, input().split())))
-n = int(input())
-
-
-def solve():
-    for i in range(L):
-        if S[i] == n:
-            return 0
-        elif S[i] > n:
-            high = S[i]
-            low = S[i - 1]
-            break
-
-    count = 0
-    for j in range(low+1, n+1):
-        # print(j)
-        for k in range(n, high):
-            if j == k == n:
-                continue
-            # print([j, k])
-            count += 1
-        # print('-----------------')
-    return count
-
-
-print(solve())
-
-
-# high와 low의 차이가 1인 것도 고려하기
-
-        
-
+p = re.compile('^[A-F]?A+F+C+[A-F]?$')
+for _ in range(int(input())):
+    print('Good' if p.match(input())==None else 'Infected!')
