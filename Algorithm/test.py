5c91ef9f5b0ee125314a9ac7cafeb05c13a51e0a
YoungIl <octover1025@naver.com>
2023-09-22 23:30:58 +0900

    0922_algo

--- Algorithm/test.py
+++ Algorithm/test.py
@@ -1,6 +1,27 @@
-import sys, re
+import sys
 input = lambda : sys.stdin.readline().rstrip()
 
-p = re.compile('^[A-F]?A+F+C+[A-F]?$')
-for _ in range(int(input())):
-    print('Good' if p.match(input())==None else 'Infected!')
+# 게임 횟수, 이긴 게임
+X, Y = map(int, input().split())
+Z = int(Y / X * 100)
+
+
+def solve(x, y, z):
+    if z >= 99:
+        return -1
+
+    start = 1
+    end = x
+
+    while start <= end:
+        mid = (start + end) // 2
+        if int((y + mid) / (x + mid) * 100) >= z:
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    return mid
+
+
+print(solve(X, Y, Z))
+
