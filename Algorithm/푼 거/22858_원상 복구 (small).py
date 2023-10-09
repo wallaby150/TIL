307ef8b54bed5c59987d0d809ff8a4aa0b1fd065
YoungIl <octover1025@naver.com>
2023-10-09 23:22:47 +0900

    1009_algo

--- Algorithm/푼 거/22858_원상 복구 (small).py
+++ Algorithm/푼 거/22858_원상 복구 (small).py
@@ -6,7 +6,7 @@ ans = list(map(int, input().split()))
 D = list(map(int, input().split()))
 
 for _ in range(K):
-    tmp = [0]*N
+    tmp = [0] * N
     for i in range(N):
         tmp[D[i]-1] = ans[i]
     ans = tmp
