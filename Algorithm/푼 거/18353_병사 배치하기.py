0c408d3a29819c063f360ef62231e08f8f906f8e
YoungIl <octover1025@naver.com>
2023-10-23 23:24:25 +0900

    1023_algo

---
+++ Algorithm/푼 거/18353_병사 배치하기.py
@@ -0,0 +1,14 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+N = int(input())
+soldiers = list(map(int, input().split()))
+
+dp = [1] * N
+
+for i in range(1, N):
+    for j in range(i):
+        if soldiers[i] < soldiers[j]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(N - max(dp))
