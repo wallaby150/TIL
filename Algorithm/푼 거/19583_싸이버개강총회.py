818917ff4402d7cdee0493ec5a3e73a6d527e19e
YoungIl <octover1025@naver.com>
2023-10-27 23:27:43 +0900

    1027_algo

---
+++ Algorithm/푼 거/19583_싸이버개강총회.py
@@ -0,0 +1,51 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+class Node(object):
+    def __init__(self, isEnd):
+        self.isEnd = isEnd
+        self.childNode = {}
+
+class Trie(object):
+    def __init__(self):
+        self.parent = Node(None)
+
+    def insert(self, string):
+        nowNode = self.parent
+        temp_length = 0
+        for char in string:
+            if char not in nowNode.childNode:
+                nowNode.childNode[char] = Node(char)
+            nowNode = nowNode.childNode[char]
+            temp_length += 1
+            if temp_length == len(string):
+                nowNode.isEnd = True
+
+    def search(self, string):
+        nowNode = self.parent
+        temp_length = 0
+        for char in string:
+            if char in nowNode.childNode:
+                nowNode = nowNode.childNode[char]
+                temp_length += 1
+                if temp_length == len(string) and nowNode.isEnd == True:
+                    return True
+            else:
+                return False
+        return False
+
+N, M = map(int, input().split())
+myTrie = Trie()
+
+for _ in range(N):
+    word = input()
+    myTrie.insert(word)
+
+result = 0
+
+for _ in range(M):
+    word = input()
+    if myTrie.search(word):
+        result += 1
+
+print(result)
