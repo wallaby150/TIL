b01bc2e414a8b34eb65f2ce287a91e4398402cbc
YoungIl <octover1025@naver.com>
2023-11-03 23:59:59 +0900

    1103_algo

---
+++ Algorithm/15489_파스칼 삼각형.py
@@ -0,0 +1,16 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+r, c, w = map(int, input().split())
+
+dp = [[1] * i for i in range(1, 32)]
+
+for i in range(2, 31):
+    for j in range(1, len(dp[i]) - 1):
+        dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
+sum = 0
+for i in range(r - 1, r + w - 1):
+    for j in range((c - 1), c + (i - r) + 1):
+        sum += dp[i][j]
+
+print(sum)
