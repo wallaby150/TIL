8ab918ff44e030520f9105b7b3ef861f21c814a4
YoungIl <octover1025@naver.com>
2023-11-13 12:27:03 +0900

    1113_algo

---
+++ Algorithm/푼 거/1080_행렬.py
@@ -0,0 +1,29 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+# 세로 N, 가로 M
+N, M = map(int, input().split())
+A = list(list(map(int, input())) for _ in range(N))
+B = list(list(map(int, input())) for _ in range(N))
+answer = 0
+
+
+def change(sy, sx):
+    for dy in range(3):
+        for dx in range(3):
+            A[sy+dy][sx+dx] = not A[sy+dy][sx+dx]
+
+
+for y in range(N-2):
+    for x in range(M-2):
+        if A[y][x] != B[y][x]:
+            change(y, x)
+            answer += 1
+
+for row_num in range(N):
+    if A[row_num] != B[row_num]:
+        print(-1)
+        break
+else:
+    print(answer)
+
