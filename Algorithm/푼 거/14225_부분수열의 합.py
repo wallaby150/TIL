5be93e8b846ac474d0d2dd3de824391d488637b2
YoungIl <octover1025@naver.com>
2023-11-18 17:28:53 +0900

    1118_algo

--- Algorithm/푼 거/14225_부분수열의 합.py
+++ Algorithm/푼 거/14225_부분수열의 합.py
@@ -1,16 +1,13 @@
 import sys
-from itertools import combinations
 input = lambda: sys.stdin.readline().rstrip()
 
 N = int(input())
 S = list(map(int, input().split()))
-answer = set(S)
+S.sort()
+target = 0
 
-for i in range(2, len(S)+1):
-    for com in combinations(S, i):
-        answer.add(sum(com))
-
-for num in range(1, 100000000001):
-    if num not in answer:
-        print(num)
+for i in S:
+    if target + 1 < i:
         break
+    target += i
+print(target+1)
