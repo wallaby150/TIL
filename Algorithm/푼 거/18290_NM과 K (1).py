1e763615b9a3405c05777c1206247a3c2ebd1f40
YoungIl <octover1025@naver.com>
2023-12-03 23:14:41 +0900

    1203_algo

---
+++ Algorithm/푼 거/18290_NM과 K (1).py
@@ -0,0 +1,34 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+# 가로 N, 세로 M
+N, M, K = map(int, input().split())
+nums = list(list(map(int, input().split())) for _ in range(N))
+visited = list([False] * M for _ in range(N))
+answer = -10000 * 101
+
+def solve(ny, nx, cnt, now_sum):
+    global answer
+
+    if cnt == K:
+        answer = max(now_sum, answer)
+        return
+
+    for y in range(ny, N):
+        for x in range(M):
+            if y == ny and x < nx:
+                continue
+
+            # 얘의 4분면이 가능한지 확인
+            for ty, tx in ((y-1, x), (y, x-1)):
+                if 0 <= ty and 0 <= tx:
+                    if visited[ty][tx]:
+                        break
+            else:
+                # 가능하다면
+                visited[y][x] = True
+                solve(y, x + 2, cnt + 1, now_sum + nums[y][x])
+                visited[y][x] = False
+
+solve(0, 0, 0, 0)
+print(answer)
