a902fb61f07bdb7c4d79c9ceaae7d11be1fc9a4a
YoungIl <octover1025@naver.com>
2024-02-21 00:25:32 +0900

    0221_algo

--- Algorithm/푼 거/2563_색종이.py
+++ Algorithm/푼 거/2563_색종이.py
@@ -3,17 +3,19 @@ input = lambda : sys.stdin.readline().rstrip()
 
 N = int(input())
 greed = [[0] * 100 for _ in range(100)]
+xm, ym = 0, 0
 ans = 0
 
 for _ in range(N):
-    y, x = map(int, input().split())
+    x, y = map(int, input().split())
+    xm, ym = max(xm, x + 10), max(ym, y + 10)
 
-    for i in range(x, x + 10):
-        for j in range(y, y + 10):
+    for i in range(y, y + 10):
+        for j in range(x, x + 10):
             greed[i][j] = 1
 
-for k in range(100):
-    for l in range(100):
+for k in range(ym):
+    for l in range(xm):
         ans += greed[k][l]
 
 print(ans)
