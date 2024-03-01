cae1582ce801bfea32f74fb716949188dd3f7a20
wallaby150 <duddlf1025@gmail.com>
2024-03-01 23:03:17 +0900

    0301_algo

---
+++ Algorithm/푼 거/10815_숫자 카드.py
@@ -0,0 +1,17 @@
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+N = int(input())
+card = list(map(int, input().split()))
+M = int(input())
+x = list(map(int, input().split()))
+
+d = {}
+for i in range(N):
+    d[card[i]] = -1
+
+for i in x:
+    if i in d:
+        print(1, end=" ")
+    else:
+        print(0, end=" ")
