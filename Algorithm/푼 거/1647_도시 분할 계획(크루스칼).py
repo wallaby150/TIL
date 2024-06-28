87e65284561bb81b8b77b8a22381a19e97565cdb
YoungIl <octover1025@naver.com>
2024-06-28 10:55:32 +0900

    0628_algo

---
+++ Algorithm/푼 거/1647_도시 분할 계획(크루스칼).py
@@ -0,0 +1,36 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+V, E = map(int, input().split())
+parents = [i for i in range(V+1)]
+ans = 0
+
+connects = [list(map(int, input().split())) for _ in range(E)]
+connects.sort(key=lambda x: x[2])
+
+
+def find(x):
+    if parents[x] == x:
+        return x
+    parents[x] = find(parents[x])
+    return parents[x]
+
+
+def union(x, y):
+    x = find(x)
+    y = find(y)
+
+    if x < y:
+        parents[y] = x
+    else:
+        parents[x] = y
+
+for connect in connects:
+    a, b, c = connect
+    if find(a) != find(b):
+        union(a, b)
+        ans += c
+        last = c
+
+
+print(ans - last)
