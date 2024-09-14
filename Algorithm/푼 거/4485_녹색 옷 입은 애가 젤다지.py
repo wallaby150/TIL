5e03bf53ee86bff7bed7ef266e6136b13ae2a43d
YoungIl <octover1025@naver.com>
2024-09-14 12:03:37 +0900

    0914_algo

---
+++ Algorithm/푼 거/4485_녹색 옷 입은 애가 젤다지.py
@@ -0,0 +1,30 @@
+import sys, heapq
+input = lambda: sys.stdin.readline().rstrip()
+
+direction = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+test_case = 0
+while True:
+    test_case += 1
+    N = int(input())
+    if not N:
+        break
+
+    grid = [list(map(int, input().split())) for _ in range(N)]
+    visited = [[float('inf')] * N for _ in range(N)]
+    visited[0][0] = grid[0][0]
+    q = [(grid[0][0], 0, 0)]
+
+    while q:
+        v, y, x = heapq.heappop(q)
+        if visited[y][x] < v:
+            continue
+
+        for dy, dx in direction:
+            ty, tx = y + dy, x + dx
+            if 0 <= ty < N and 0 <= tx < N:
+                tv = v + grid[ty][tx]
+                if visited[ty][tx] > tv:
+                    visited[ty][tx] = tv
+                    heapq.heappush(q, (tv, ty, tx))
+
+    print(f'Problem {test_case}: {visited[-1][-1]}')
