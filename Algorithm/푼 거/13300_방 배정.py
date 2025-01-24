9455f8c7dc3f0a3bac670e9b28aa6afb79c7adf6
wallaby150 <duddlf1025@gmail.com>
2025-01-24 22:21:22 +0900

    250124_algo

--- Algorithm/푼 거/13300_방 배정.py
+++ Algorithm/푼 거/13300_방 배정.py
@@ -1,5 +1,16 @@
-N = int(input())
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+N, K = map(int, input().split())
+rooms = [[0] * 2 for _ in range(6)]
 
 for _ in range(N):
-    print('* ' * (N - N // 2))
-    print(' *' * (N // 2))
+    S, Y = map(int, input().split())
+    rooms[Y - 1][S] += 1
+
+total_rooms = 0
+for grade in range(6):
+    for gender in range(2):
+        total_rooms += (rooms[grade][gender] + K - 1) // K
+
+print(total_rooms)
